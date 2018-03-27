db3caee21aa4b7336cb732c0d9394ca320907374
Michael Foiani <mfoiani2019@communityschoolnaples.org>
2018-03-26 22:52:18 -0400

    Updated webiste for 2018 competition. Changed years, schedule, and other small aspects.

--- schedule.php
+++ schedule.php
@@ -48,14 +48,14 @@ require_once "includes/require.php";
                         <p><span class="glyphicon glyphicon-calendar icon-font" aria-hidden="true"></span><br>WHEN</p>
                     </div>
                     <div class="col-lg-6 col-sm-6 col-xs-6 text-left">
-                        <p class="lead">The competition will take place on <strong>Saturday, April 22rd, 2017</strong>. Mark your calendars!<br>Don't forget to sign up by April 12th!</p>
+                        <p class="lead">The competition will take place on <strong>Saturday, April 7th, <?php echo date("Y") ?></strong>. Mark your calendars!<br>Don't forget to sign up by April 6th!</p>
                     </div>
                 </div>
 
                 <div class="row info-row">
                     <div class="col-lg-6 text-center">
                         <p>
-                            <span class="large-text">9:00 - 9:20</span>
+                            <span class="large-text">8:45 - 9:00</span>
                         </p>
                     </div>
                     <div class="col-lg-6">
@@ -67,7 +67,7 @@ require_once "includes/require.php";
                 <div class="row info-row">
                     <div class="col-lg-6 text-center">
                         <p>
-                            <span class="large-text">9:20 - 9:35</span>
+                            <span class="large-text">9:00 - 9:10</span>
                         </p>
                     </div>
                     <div class="col-lg-6">
@@ -78,20 +78,34 @@ require_once "includes/require.php";
                 <div class="row info-row">
                     <div class="col-lg-6 text-center">
                         <p>
-                            <span class="large-text">9:40 - 10:00</span>
+                            <span class="large-text">9:15 - 9:25</span>
                         </p>
                     </div>
                     <div class="col-lg-6">
-                        <p class="lead text-left">Warm up round. This is the first round of the programming competition, where
+                        <p class="lead text-left">Tutorial round. This is the first round of the programming competition, where
                             teams will familiarize themselves with the equipment and questions may be asked. This round is not
-                            scored, and is just for solving any technical issues.</p>
+                            scored, and is just for solving any technical issues. We will not proceed to the next round until
+                            every contestant has correctly answered each question in this round.</p>
                     </div>
                 </div>
 
                 <div class="row info-row">
                     <div class="col-lg-6 text-center">
                         <p>
-                            <span class="large-text">10:05 - 12:45</span>
+                            <span class="large-text">9:25 - 9:45</span>
+                        </p>
+                    </div>
+                    <div class="col-lg-6">
+                        <p class="lead text-left">Warm-up round. This round of the competition will consists of very easy problems,
+                        which confirms that every contestant understands how to take inputs and submit. This round is also not
+                            scored, and we will not proceed to the speed round until everyone has completed these questions.</p>
+                    </div>
+                </div>
+
+                <div class="row info-row">
+                    <div class="col-lg-6 text-center">
+                        <p>
+                            <span class="large-text">9:50 - 10:30</span>
                         </p>
                     </div>
                     <div class="col-lg-6">
@@ -104,24 +118,27 @@ require_once "includes/require.php";
                 <div class="row info-row">
                     <div class="col-lg-6 text-center">
                         <p>
-                            <span class="large-text">12:50 - 1:25</span>
+                            <span class="large-text">10:40 - 11:20</span>
                         </p>
                     </div>
                     <div class="col-lg-6">
-                        <p class="lead text-left">Lunch! Pizza will be provided by Gulfstream Painting.</p>
+                        <p class="lead text-left">AP round. This round of the competition will consist of multiple problems based
+                            off past AP exam FRQs. These questions will have many steps to them, will take much longer to complete,
+                            and will match the difficulty of an AP exam. A correct response along with speed will determine the amount
+                            of points received for each problem.</p>
                     </div>
                 </div>
 
                 <div class="row info-row">
                     <div class="col-lg-6 text-center">
                         <p>
-                            <span class="large-text">1:30 - 2:30</span>
+                            <span class="large-text">11:30 - 12:00</span>
                         </p>
                     </div>
                     <div class="col-lg-6">
-                        <p class="lead text-left">Mutliple Choice Round. Each team will be expected to complete
-                            a AP style multiple choice test. These questions will be taken from past AP exams and
-                            created by AP teachers or former AP Comper Science A students. It is not a huge factor
+                        <p class="lead text-left">Multiple Choice Round. Each team will be expected to complete
+                            an AP style multiple choice test. These questions will be taken from past AP exams and
+                            created by AP teachers or former AP Computer Science A students. It is not a huge factor
                             for score, but it will be scored. </p>
                     </div>
                 </div>
@@ -129,25 +146,14 @@ require_once "includes/require.php";
                 <div class="row info-row">
                     <div class="col-lg-6 text-center">
                         <p>
-                            <span class="large-text">2:30 - 2:45</span>
+                            <span class="large-text">12:10 - 12:30</span>
                         </p>
                     </div>
                     <div class="col-lg-6">
-                        <p class="lead text-left">Nap Time! Programming gets tough, and we understand that struggle!<br><br></p>
-                    </div>
-                </div>
-
-                <div class="row info-row">
-                    <div class="col-lg-6 text-center">
-                        <p>
-                            <span class="large-text">2:45 - 3:00</span>
-                        </p>
-                    </div>
-                    <div class="col-lg-6">
+                        <p class="lead text-left">Lunch! Pizza will be provided by Gulfstream Painting.</p>
                         <p class="lead text-left">Awards, prizes, and then your parents will pick you up.</p>
                     </div>
                 </div>
-            </div>
         </div>
 
 
