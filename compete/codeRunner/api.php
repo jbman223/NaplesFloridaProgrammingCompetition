c2c45895819214025b13756f6994e541d3ba7f94
Jacob Buckheit <jbman223@gmail.com>
2018-03-27 12:48:09 -0400

    update new compilebox

--- compete/codeRunner/api.php
+++ compete/codeRunner/api.php
@@ -1,20 +1,20 @@
-<?php
+\<?php
 require_once "../content/require.php";
 require_once "../../library/Requests.php";
 ignore_user_abort(true);
 Requests::register_autoloader();
 
 
-$languages = array("java" => 8);
+$languages = array("java" => "java", "javascript" => "javascript", "c" => "c");
 
 if (isset($_POST['code'], $_POST['input'], $_POST['language'])) {
-    $url = "http://159.203.168.143/compile";
+    $url = "http://159.203.191.58:31337/eval/";
     $code = str_replace("public class", "class", $_POST['code']);
     $language = $languages[$_POST['language']];
     $input = $_POST['input'];
 
     $header = array('Content-Type' => 'application/json');
-    $response = Requests::post($url, $header, json_encode(array("language" => $language, "code" => $code, "stdin" => $input)));
+    $response = Requests::post($url, $header, json_encode(array("language" => $language, "code" => $code, "stdins" => explode("\n", $input))));
     die($response->body);
 }
 
--- compete/codeRunner/internalRun.php
+++ compete/codeRunner/internalRun.php
@@ -5,14 +5,14 @@ Requests::register_autoloader();
 
 function run($lang, $input, $code)
 {
-    $languages = array("java" => 8, "c" => 7);
+    $languages = array("java" => "java", "javascript" => "javascript", "c" => "c");
 
-    $url = "http://159.203.168.143/compile";
+    $url = "http://159.203.191.58:31337/eval/";
     $code = str_replace("public class", "class", $code);
     $language = $languages[$lang];
 
     $header = array('Content-Type' => 'application/json');
-    $response = Requests::post($url, $header, json_encode(array("language" => $language, "code" => $code, "stdin" => $input)));
+    $response = Requests::post($url, $header, json_encode(array("language" => $language, "code" => $code, "stdins" => explode("\n", $input))));
 
     //$body = json_decode($response->body);
     //$body->errors = preg_replace('/Note:.+/g', '', $body->errors);
