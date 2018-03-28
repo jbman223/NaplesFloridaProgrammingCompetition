7cdc77e1e719228b343d76dcd2deff2614e07058
Jacob Buckheit <jbman223@gmail.com>
2018-03-27 22:30:25 -0400

    change

--- autodeploy.php
+++ autodeploy.php
@@ -5,9 +5,10 @@ $event = $_SERVER['HTTP_X_GITHUB_EVENT'];
 $guid = $_SERVER['HTTP_X_GITHUB_DELIVERY'];
 
 if ($event != "push" && $event != "ping") {
-    echo $event;
     die("Unsupported event.");
 }
+
+
 ob_start();
 var_dump($_POST);
 $log = fopen("gitlog.txt", "w+");
