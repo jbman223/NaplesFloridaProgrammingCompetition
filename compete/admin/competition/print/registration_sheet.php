afb488cacac992560ab10999ce481f6dae62afa5
Jacob Buckheit <jbman223@gmail.com>
2018-04-04 11:47:46 -0400

    registration sheet includes school and member names.

--- compete/admin/competition/print/registration_sheet.php
+++ compete/admin/competition/print/registration_sheet.php
@@ -8,7 +8,7 @@ if (!$user['admin']) {
     header("Location: ../../../account/login.php");
 }
 
-$state = $db->prepare("SELECT team_name, backendLogin, backendPassword FROM `teams` where admin = 0 and deleted = 0");
+$state = $db->prepare("SELECT c.f_name, c.l_name, c.school, t.team_name, t.backendLogin, t.backendPassword FROM competitors c JOIN `teams` ON c.team_id = t.id where t.admin = 0 and t.deleted = 0 and c.deleted = 0 ORDER BY t.team_name");
 $state->execute();
 $teams = $state->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -38,6 +38,9 @@ $teams = $state->fetchAll(PDO::FETCH_ASSOC);
 <table class="table">
     <thead>
         <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>School</th>
             <th>Team Name</th>
             <th>Login</th>
             <th>Password</th>
@@ -49,6 +52,9 @@ $teams = $state->fetchAll(PDO::FETCH_ASSOC);
     foreach ($teams as $team) {
         ?>
     <tr>
+        <td><?php echo $team["f_name"]; ?></td>
+        <td><?php echo $team["l_name"]; ?></td>
+        <td><?php echo $team["school"]; ?></td>
         <td><?php echo $team["team_name"]; ?></td>
         <td><?php echo $team["backendLogin"]; ?></td>
         <td><?php echo $team["backendPassword"]; ?></td>
